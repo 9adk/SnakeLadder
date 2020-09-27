712bbd57510a7efff359de73038d76e0d4dd60c8
aditya Kharade <adityakharade51@gmail.com>
2020-09-27 22:11:09 +0530

    Snake Ladder for two players

--- src/SnakeLadderMain.java
+++ src/SnakeLadderMain.java
@@ -4,17 +4,12 @@ public class SnakeLadderMain {
 	public static final int NO_PLAYS = 0;
 	public static final int LADDER = 1;
 	public static final int SNAKE = 2;
-
-	public static void main(String[] args) {
-		// Variables
-		int position = 0;
+	public static int snakeLadder(int position,String Player ) {
 		int number =0;
 		int option=0;
-		int rolling_die =0;
-		do{
+		
 		    number =(int) Math.floor((Math.random()*10))%7;
 			option = (int) Math.floor((Math.random()*10))%3;
-			rolling_die++;
 			switch(option) {
 			case NO_PLAYS: 
 				break;
@@ -22,8 +17,10 @@ public class SnakeLadderMain {
 			position=position + number;
 			if(position>100) {
 				position = position-number;
-			}
-			
+			}	
+				if(position!=100) {
+					snakeLadder(position,Player);
+					}
 			break;
 			case SNAKE: 
 			position=position-number;
@@ -32,10 +29,34 @@ public class SnakeLadderMain {
 				}
 			break;
 			}
-			System.out.println("Position is "+ position);
-		}
-		while(position!=100);
-		System.out.println("no of times die rolled:"+ rolling_die);
+			System.out.println("Position of "+Player+" is "+position);
+		return position;
 
+	}
+	public static void main(String[] args) {
+		int positionOfP1 = 0;
+		int positionOfP2 = 0;
+		boolean turn = false;
+		int x = (int) (Math.floor(Math.random()*2));
+		if(x == 1) {
+			turn =true;
+		}
+		while(positionOfP1 <100 && positionOfP2<100) {
+		   
+			if(turn) {
+				positionOfP1 = snakeLadder(positionOfP1,"Player 1");
+			}
+			else {
+				positionOfP2 = snakeLadder(positionOfP2,"Player 2");
+			}
+			
+			turn = !turn ;
+		}
+		if(positionOfP1 == 100) {
+			System.out.println("Player 1 wins");
+		}
+		else {
+			System.out.println("Player 2 wins");
+		}
 	}
 }
