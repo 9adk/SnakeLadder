1679e20174ba4a2ce285ec9ed75d187311e87545
aditya Kharade <adityakharade51@gmail.com>
2020-09-27 20:06:39 +0530

    Generating random number

--- src/SnakeLadderMain.java
+++ src/SnakeLadderMain.java
@@ -6,5 +6,7 @@ public class SnakeLadderMain {
 	public static void main(String[] args) {
 		// Variables
 		int postion = 0;
+		int number =0;
+		number = (int) ( Math.floor(Math.random()*10)%6)+1;
 	}
 }
